1f695d76e5e918a567954514c1e4dca3c34d2f05
Brian Zoetewey <brian.zoetewey@ccci.org>
2013-10-01 16:51:22 +0000

    Includes Authentication of users with CAS and user email invite

---
+++ api/callback.php
@@ -0,0 +1,12 @@
+<?php
+	define('WP_USE_THEMES', false);
+	require_once( realpath( $_SERVER[ 'DOCUMENT_ROOT' ] . '/wp-load.php' ) );
+	\GlobalTechnology\CentralAuthenticationService\CASLogin::singleton()->get_cas_client()->forceAuthentication();
+?>
+<html>
+	<head>
+		<title>CAS Callback Handler</title>
+	</head>
+	<body>
+	</body>
+</html>
