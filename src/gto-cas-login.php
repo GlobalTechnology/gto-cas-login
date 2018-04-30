6acffb088348a065464ac323ed0eb661a5dd9712
Brian Zoetewey <brian.zoetewey@ccci.org>
2018-04-30 17:08:36 -0400

    Extend logged_in_cookie by 1 week.

--- src/gto-cas-login.php
+++ src/gto-cas-login.php
@@ -552,7 +552,7 @@ namespace {
     if ( false === $logged_in ) {
       setcookie( 'logged_in_cookie', ' ', time() - YEAR_IN_SECONDS, COOKIEPATH, ltrim(COOKIE_DOMAIN, '.') );
     } else {
-      setcookie( 'logged_in_cookie', 'logged_in', time() + 2 * HOUR_IN_SECONDS, COOKIEPATH, ltrim(COOKIE_DOMAIN, '.'), true, true );
+      setcookie( 'logged_in_cookie', 'logged_in', time() + WEEK_IN_SECONDS, COOKIEPATH, ltrim(COOKIE_DOMAIN, '.'), true, true );
     }
 
     return $logged_in;
